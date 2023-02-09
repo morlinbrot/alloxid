9d861d7418b4459b13ca5542a55bf250950e4d32
morlinbrot <morlinbrot@mailbox.org>
2023-02-09 19:01:17 +0100

    Add a create_action to HealthCheck

--- alloxid-front/src/app/health_check.rs
+++ alloxid-front/src/app/health_check.rs
@@ -1,7 +1,21 @@
 use leptos::*;
 
-async fn health_check(_how_many: i32) -> Option<String> {
+async fn health_check(_: ()) -> Option<String> {
     let client = reqwest::Client::new();
+    client
+        .get("http://localhost:3000/health-check")
+        .send()
+        .await
+        .map_err(|e| log::error!("{e}"))
+        .ok()?
+        .text()
+        .await
+        .ok()
+}
+
+async fn health_check2() -> Option<String> {
+    let client = reqwest::Client::new();
+    log!("FETCHING...");
     client
         .get("http://localhost:3000/health-check")
         .send()
@@ -15,21 +29,30 @@ async fn health_check(_how_many: i32) -> Option<String> {
 
 #[component]
 pub fn HealthCheck(cx: Scope) -> impl IntoView {
-    let (how_many, set_how_many) = create_signal(cx, 1);
+    let message = create_resource(cx, || (), health_check);
 
-    let message = create_resource(cx, how_many, health_check);
-    // let _: () = message;
+    let fetch_message = create_action(cx, |_: &()| health_check2());
+    let handle_click = move |_| fetch_message.dispatch(());
+
+    let (count, set_count) = create_signal(cx, 0);
+    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
         cx,
         <div>
-            <p>"Health check:"</p>
-            <p>
-                { move || message.read().map(|msg| match msg {
-                    None => format!("Failed to fetch message. Is the backend running?"),
-                    Some(msg) => format!("{msg}"),
+            <button on:click=on_click>"Click Me: " {count}</button>
+
+            <h4>"Health click:"</h4>
+            <button on:click=handle_click>"Fetch message"</button>
+            <p>"Message: "{fetch_message.value()}</p>
+
+            <h4>"Health check:"</h4>
+            <Suspense fallback=|| view! { cx, "Loading..." }>
+                {move || message.read().map(|msg| match msg {
+                    None => view! { cx,  <div>"Error fetching the message. Is the backend running?"</div> },
+                    Some(msg) => view! { cx, <div>{msg}</div> }
                 })}
-            </p>
+            </Suspense>
         </div>
     }
 }
